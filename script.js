717f519474c012e3c5fbe275f4a1e848b0543274
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-09-04 15:18:47 +0200

    esercizio completato

---
+++ script.js
@@ -0,0 +1,11 @@
+//Seleziono gli elementi dal DOM usando il loro id
+
+const lamp = document.getElementById('lampadina');
+const button = document.getElementById('bottoneLuce');
+
+//Aggiungo una funzione, quando qualcuno clicca sull'elemento "button" esegue il comando
+
+button.addEventListener('click', function() {
+    lamp.src = "img/yellow_lamp.png";
+});
+
